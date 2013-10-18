09f84cf0ffce401f2538a522324499789def47d4
TheGreenie123 <gam_eacount@hotmail.com>
2013-10-18 17:10:18 +1100

    Added a technique that should be used to find the largest prime

--- NO-3.cs
+++ NO-3.cs
@@ -11,6 +11,22 @@ What is the largest prime factor of the number 600851475143 ? */
        must be as optimised as possible, i will sit on the challenge for a while,
        whilst i absorb some math on factoring :) */
 
+    /* Example 2: What is the prime factorization of 147 ?
+
+Can we divide 147 evenly by 2? No, so we should try the next prime number, 3:
+
+147 ÷ 3 = 49
+
+Then we try factoring 49, and find that 7 is the smallest prime number that works:
+
+49 ÷ 7 = 7
+
+And that is as far as we need to go, because all the factors are prime numbers.
+
+147 = 3 × 7 × 7
+
+(or 147 = 3 × 72 using exponents) */
+
     static void Main() {
 
 
