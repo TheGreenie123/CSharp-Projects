56c203ed3a1228ef21d8f981857ff67c1bcd23e1
TheGreenie123 <gam_eacount@hotmail.com>
2013-10-18 15:59:20 +1100

    NO-3 incomplete but in progress

---
+++ NO-3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class YOlo {
+   /* The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ? */
+
+    static void Main() {
+
+        Console.WriteLine(check.if_prime(10007));
+        Console.ReadKey();
+    }
+}
+
+static class check {
+    public static bool if_prime(ulong number) {
+
+        bool returnholder = true;
+
+        for (ulong x = 2; x < number; x++) {
+            if (number % x == 0)
+            {
+                returnholder = false;
+            }
+        }
+        return returnholder;
+    }
+}
